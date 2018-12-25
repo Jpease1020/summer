ce4d7a240856158958e6a2ddb49d208b04e2b811
Justin Pease <jpea4@allstate.com>
2018-12-24 21:06:13 -0500

    removes yarl lock file

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{marginTop: '60px'}} className="App">
+      <img src="https://media.giphy.com/media/gNke2UrUTopOg/giphy.gif" height="200" width="200"></img>
+      <h1>MERRY CHRISTMAS!!!!!!!!!</h1>
+        <div style={{margin: '60px'}}>Hi Summer, I wasn't able to get you a puppy in time for Christmas but I do pleadge to buy you a puppy. I was thinking around $350 like you originally said but that's flexible.
+        I also got you a small gift card to Kendra Scott but I bought it online so I'll forawrd that to you via email!
+        Merry Christmas Snuggle Pie Baby Bird!!!!!!!!!!!!!!!!!!!!!!
+        </div>
+        <img src="https://media.giphy.com/media/JazIWzRasLEn6/giphy.gif" height="200" width="200"></img>
+        <img src="https://media.giphy.com/media/qPuhFBQt8xLEY/giphy.gif" height="200" width="200"></img>
+        <img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif" height="200" width="200"></img>
+        <img src="https://media.giphy.com/media/zPGJzH8fXsoBW/giphy.gif" height="200" width="200"></img>
+        <img src="https://media.giphy.com/media/fvzzTw4GevP2g/giphy.gif" height="200" width="200"></img>
       </div>
     );
   }
