b7df9d5e0654cea2dc35385fc1a7e867e60d1892
Justin Pease <jpea4@allstate.com>
2018-12-24 21:10:13 -0500

    modifies text

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
       <div style={{marginTop: '60px'}} className="App">
       <img src="https://media.giphy.com/media/gNke2UrUTopOg/giphy.gif" height="200" width="200"></img>
       <h1>MERRY CHRISTMAS!!!!!!!!!</h1>
-        <div style={{margin: '60px'}}>Hi Summer, I wasn't able to get you a puppy in time for Christmas but I do pleadge to buy you a puppy. I was thinking around $350 like you originally said but that's flexible.
-        I also got you a small gift card to Kendra Scott but I bought it online so I'll forawrd that to you via email!
-        Merry Christmas Snuggle Pie Baby Bird!!!!!!!!!!!!!!!!!!!!!!
+        <div style={{margin: '60px'}}>Hi Summer, I wasn't able to get you a puppy in time for Christmas
+        but I do pledge to buy you a puppy. I was thinking around $350 like you originally said but
+        that's flexible. I also got you a small gift card to Kendra Scott but I bought it online so
+        I'll forward that to you via email! Merry Christmas Snuggle Pie Baby Bird!!!!!!!!!!!!!!!!!!!!!!
         </div>
         <img src="https://media.giphy.com/media/JazIWzRasLEn6/giphy.gif" height="200" width="200"></img>
         <img src="https://media.giphy.com/media/qPuhFBQt8xLEY/giphy.gif" height="200" width="200"></img>
